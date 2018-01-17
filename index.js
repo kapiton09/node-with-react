668285f265f0f7b5cae669abd7d64b0fd6cba44b
Kapil Shrestha <contact@kapilshrestha.com>
2018-01-17 21:13:38 +1300

    finished auth flow

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
-//mongoose.connect(keys.mongoURI);
-// Using `mongoose.connect`...
-var promise = mongoose.connect(keys.mongoURI, {
-  useMongoClient: true,
-  /* other options */
-});
-// Or `createConnection`
-var promise = mongoose.createConnection(keys.mongoURI, {
-  useMongoClient: true,
-  /* other options */
-});
+mongoose.connect(keys.mongoURI, { useMongoClient: true});
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
